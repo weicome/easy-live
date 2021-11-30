fb6ec02b378dda8c4fecac80929f399a2d3609e0
weicome <407888190@qq.com>
2021-11-30 10:47:57 +0800

    [Mod] 修改关闭进程方法

--- App/HttpController/Api.php
+++ App/HttpController/Api.php
@@ -12,60 +12,67 @@ class Api extends BaseController
     // 创建一个直播流
     public function create()
     {
-        try{
+        try {
             $data = $this->raw();
-            if(empty($data)){
-                throw new \Exception('参数错误,live_host是必须的',400);
+            if (empty($data)) {
+                throw new \Exception('参数错误,live_host是必须的', 400);
             }
-            $data = json_decode($data,true);
-            if(!isset($data['live_host'])||empty($data['live_host'])){
-                throw new \Exception('参数错误,live_host是必须的',400);
+            $data = json_decode($data, true);
+            if (!isset($data['live_host']) || empty($data['live_host'])) {
+                throw new \Exception('参数错误,live_host是必须的', 400);
             }
             $app = $this->input('live') ?: 'live';
             $live_host = $data['live_host'];
             $stream_id = $this->input('stream') ?: md5($live_host);
-            $stream_key = stream_key($app,$stream_id);
+            $stream_key = stream_key($app, $stream_id);
             $streamTable = TableManager::getInstance()->get('stream');
-            if($row = $streamTable->get($stream_key)){
-                $row = json_decode($row['rows'],true);
+            if ($row = $streamTable->get($stream_key)) {
+                $row = json_decode($row['rows'], true);
                 $play = true;
-            }else{
-                $streamTable->set($stream_key,['rows'=>json_encode(['app'=>$app,'stream_id'=>$stream_id,'rtsp_host'=>$live_host])]);
+            } else {
+                $streamTable->set($stream_key, ['rows' => json_encode(['app' => $app, 'stream_id' => $stream_id, 'rtsp_host' => $live_host])]);
                 $process =  Di::getInstance()->get('ffmProcess');
                 $play = $process->write($stream_key);
-                $row = ['app'=>$app,'stream_id'=>$stream_id,'rtsp_host'=>$live_host];
+                $row = ['app' => $app, 'stream_id' => $stream_id, 'rtsp_host' => $live_host];
             }
             $localhost = Config::getInstance()->getConf('srs.localhost');
             $httpport = Config::getInstance()->getConf('srs.srs_config.http_server.listen');
-            $httpport = substr($httpport,0,strpos($httpport,';'));
+            $httpport = substr($httpport, 0, strpos($httpport, ';'));
             $play ? $this->success([
                 'stream_key' => $stream_key,
-                'RTMP'=> "rtmp://{$localhost}/{$row['app']}/{$row['stream_id']}",
-                'HTTP-FLV'=> "http://{$localhost}:{$httpport}/{$row['app']}/{$row['stream_id']}.flv",
-                'HLS'=> "http://{$localhost}:{$httpport}/{$row['app']}/{$row['stream_id']}.m3u8",
-                'WebRTC'=> "webrtc://{$localhost}/{$row['app']}/{$row['stream_id']}",
-            ],'播放成功') : $this->error();
-        }catch (\Exception $exception){
+                'RTMP' => "rtmp://{$localhost}/{$row['app']}/{$row['stream_id']}",
+                'HTTP-FLV' => "http://{$localhost}:{$httpport}/{$row['app']}/{$row['stream_id']}.flv",
+                'HLS' => "http://{$localhost}:{$httpport}/{$row['app']}/{$row['stream_id']}.m3u8",
+                'WebRTC' => "webrtc://{$localhost}/{$row['app']}/{$row['stream_id']}",
+            ], '播放成功') : $this->error();
+        } catch (\Exception $exception) {
             throw $exception;
         }
     }
 
     public function destroy()
     {
-        try{
-            $data = json_decode($this->raw(),true);
-            if(empty($data) || !isset($data['stream_key']) || empty($data['stream_key'])){
-                throw new \Exception('参数错误',400);
+        try {
+            $data = json_decode($this->raw(), true);
+            if (empty($data) || !isset($data['stream_key']) || empty($data['stream_key'])) {
+                throw new \Exception('参数错误', 400);
             }
             $stream_key = $data['stream_key'];
-            $processTable = TableManager::getInstance()->get('stream');
-            if($processTable->exist($stream_key)){
+            $streamTable = TableManager::getInstance()->get('stream');
+            if ($streamTable->exist($stream_key)) {
                 $process =  Di::getInstance()->get('ffmProcess');
-                $process->write('close-'.$stream_key);
-                $processTable->del($stream_key);
+                $process->write('close-' . $stream_key);
+                $streamTable->del($stream_key);
+                $processTable = TableManager::getInstance()->get('process');
+                if ($php_pid = $processTable->get($stream_key, 'php_pid')) {
+                    echo '进程ID: ' . $php_pid . PHP_EOL;
+                    \swoole_process::kill($php_pid, 0);
+                    exec("pkill -P {$php_pid}");
+                    exec("kill -9 {$php_pid}");
+                }
             }
-            $this->success([],'删除成功');
-        }catch (\Exception $exception){
+            $this->success([], '删除成功');
+        } catch (\Exception $exception) {
             throw  $exception;
         }
     }
--- App/Process/FFmpegProcess.php
+++ App/Process/FFmpegProcess.php
@@ -53,6 +53,9 @@ class FFmpegProcess extends AbstractProcess
         $process = proc_open($cmd,  $descriptorspec, $pipes);
         $this->childProc[$stream_key] = [$process, $pipes];
         echo $process . PHP_EOL;
+        $pinfo = json_encode(proc_get_status($process));
+        $processTable = TableManager::getInstance()->get('process');
+        $processTable->set($stream_key, ['php_pid' => $pinfo['pid']]);
     }
 
     protected function onPipeReadable(Process $process)
@@ -64,10 +67,10 @@ class FFmpegProcess extends AbstractProcess
             if (strlen($stream_key) > 32) {
                 $msg = explode('-', $stream_key);
                 if (list($proc, $pipes) = $this->childProc[$msg[1]]) {
-                    fclose($pipes[0]);
-                    fclose($pipes[1]);
-                    fclose($pipes[2]);
-                    $res = proc_close($proc);
+                    if (isset($pipes[0])) fclose($pipes[0]);
+                    if (isset($pipes[1])) fclose($pipes[1]);
+                    if (isset($pipes[2])) fclose($pipes[2]);
+                    $res = proc_terminate($proc);
                     echo '退出状态:' . $res . PHP_EOL;
                 }
             } else {
